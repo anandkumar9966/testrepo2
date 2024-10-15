3a91ea26c416c566f17fed7b708f505d4123300f
anandkumar9966 <61936360+anandkumar9966@users.noreply.github.com>
2024-10-15 15:31:54 +0530

    Delete demo.py

--- demo.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python3
-import random;
-name=input("enter your name");
-l=["sunny","funny","chinny", "gummy","vammy"];
-print ("hello", name);
-print("congratulations you are going to marry:", random.choice(l))
