61d17e886b9948558c034ab972ec1ee56f9470d5
Anand kumar <movidianand123@gmail.com>
2024-10-15 14:25:22 +0530

    this is a python program

---
+++ demo.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+import random;
+name=input("enter your name");
+l=["sunny","funny","chinny", "gummy","vammy"];
+print ("hello", name);
+print("congratulations you are going to marry" random.input(l));
