7b79e26bab81251d031f541484302577b0e06148
Anand kumar <movidianand123@gmail.com>
2024-10-15 14:27:32 +0530

    this is a python programme

--- demo.py
+++ demo.py
@@ -3,4 +3,4 @@ import random;
 name=input("enter your name");
 l=["sunny","funny","chinny", "gummy","vammy"];
 print ("hello", name);
-print("congratulations you are going to marry" random.input(l));
+print("congratulations you are going to marry" random.choice(l))
